8f7638fe4b92bc8cebb66dd4308908d7f67273f2
Brandon Fries <brandonfries1993@gmail.com>
2019-07-16 17:04:28 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,7 +2,16 @@ function getIt() {
   $('p').on("click", function() {
     alert("hey!")
   })
-}//define functions here
+}
+
+function frameIt(){
+  $("img").on('load', function() {
+    $(this).css('border', "solid 2px red").addClass('tasty')
+  })
+}
+
+
+
 
 $(document).ready(function(){
 
